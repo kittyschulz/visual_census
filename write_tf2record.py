5ad8b128f435757f6783883ddfc7d08aa39be581
Katerina <kittyschulz@gmail.com>
2019-10-15 12:04:57 -0700

    initial commit of write_tf2record.py

---
+++ write_tf2record.py
@@ -0,0 +1,35 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# Import system helpers
+import os
+import pathlib
+from os import listdir
+from os.path import isfile, join
+
+# Import TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import datasets, layers, models
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# Import helper libraries
+import numpy as np
+import pandas as pd
+import scipy.io
+import matplotlib.pyplot as plt
+import IPython.display as display
+from PIL import Image
+
+def _bytes_feature(value):
+  """Returns a bytes_list from a string / byte."""
+  if isinstance(value, type(tf.constant(0))):
+    value = value.numpy() # BytesList won't unpack a string from an EagerTensor.
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+def _float_feature(value):
+  """Returns a float_list from a float / double."""
+  return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
+def _int64_feature(value):
+  """Returns an int64_list from a bool / enum / int / uint."""
+  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
