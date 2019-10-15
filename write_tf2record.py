d60f4671e08989cef72be7fdbd96a727f1c9d917
Katerina <kittyschulz@gmail.com>
2019-10-15 13:31:22 -0700

    changed pipeline to resize images

--- write_tf2record.py
+++ write_tf2record.py
@@ -9,8 +9,6 @@ from os.path import isfile, join
 # Import TensorFlow and tf.keras
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import datasets, layers, models
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 # Import helper libraries
 import numpy as np
