5745cc888821c32d449a4a80c8385bd7277fa65b
Katerina <kittyschulz@gmail.com>
2019-11-10 11:12:21 -0800

    changed directory stucture

---
+++ image_classifier/resnet_152.py
@@ -0,0 +1,241 @@
+import keras
+from keras.optimizers import SGD
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, AveragePooling2D, ZeroPadding2D, Flatten, Activation, add
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model
+from keras import backend as K
+
+from sklearn.metrics import log_loss
+
+from custom_layers.scale_layer import Scale
+
+import sys
+sys.setrecursionlimit(3000)
+
+
+def identity_block(input_tensor, kernel_size, filters, stage, block):
+    '''The identity_block is the block that has no conv layer at shortcut
+    # Arguments
+        input_tensor: input tensor
+        kernel_size: defualt 3, the kernel size of middle conv layer at main path
+        filters: list of integers, the nb_filters of 3 conv layer at main path
+        stage: integer, current stage label, used for generating layer names
+        block: 'a','b'..., current block label, used for generating layer names
+    '''
+    eps = 1.1e-5
+    nb_filter1, nb_filter2, nb_filter3 = filters
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+    scale_name_base = 'scale' + str(stage) + block + '_branch'
+
+    x = Conv2D(nb_filter1, (1, 1), name=conv_name_base +
+               '2a', use_bias=False)(input_tensor)
+    x = BatchNormalization(epsilon=eps, axis=bn_axis,
+                           name=bn_name_base + '2a')(x)
+    x = Scale(axis=bn_axis, name=scale_name_base + '2a')(x)
+    x = Activation('relu', name=conv_name_base + '2a_relu')(x)
+
+    x = ZeroPadding2D((1, 1), name=conv_name_base + '2b_zeropadding')(x)
+    x = Conv2D(nb_filter2, (kernel_size, kernel_size),
+               name=conv_name_base + '2b', use_bias=False)(x)
+    x = BatchNormalization(epsilon=eps, axis=bn_axis,
+                           name=bn_name_base + '2b')(x)
+    x = Scale(axis=bn_axis, name=scale_name_base + '2b')(x)
+    x = Activation('relu', name=conv_name_base + '2b_relu')(x)
+
+    x = Conv2D(nb_filter3, (1, 1), name=conv_name_base +
+               '2c', use_bias=False)(x)
+    x = BatchNormalization(epsilon=eps, axis=bn_axis,
+                           name=bn_name_base + '2c')(x)
+    x = Scale(axis=bn_axis, name=scale_name_base + '2c')(x)
+
+    x = add([x, input_tensor], name='res' + str(stage) + block)
+    x = Activation('relu', name='res' + str(stage) + block + '_relu')(x)
+    return x
+
+
+def conv_block(input_tensor, kernel_size, filters, stage, block, strides=(2, 2)):
+    '''conv_block is the block that has a conv layer at shortcut
+    # Arguments
+        input_tensor: input tensor
+        kernel_size: defualt 3, the kernel size of middle conv layer at main path
+        filters: list of integers, the nb_filters of 3 conv layer at main path
+        stage: integer, current stage label, used for generating layer names
+        block: 'a','b'..., current block label, used for generating layer names
+    Note that from stage 3, the first conv layer at main path is with subsample=(2,2)
+    And the shortcut should have subsample=(2,2) as well
+    '''
+    eps = 1.1e-5
+    nb_filter1, nb_filter2, nb_filter3 = filters
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+    scale_name_base = 'scale' + str(stage) + block + '_branch'
+
+    x = Conv2D(nb_filter1, (1, 1), strides=strides,
+               name=conv_name_base + '2a', use_bias=False)(input_tensor)
+    x = BatchNormalization(epsilon=eps, axis=bn_axis,
+                           name=bn_name_base + '2a')(x)
+    x = Scale(axis=bn_axis, name=scale_name_base + '2a')(x)
+    x = Activation('relu', name=conv_name_base + '2a_relu')(x)
+
+    x = ZeroPadding2D((1, 1), name=conv_name_base + '2b_zeropadding')(x)
+    x = Conv2D(nb_filter2, (kernel_size, kernel_size),
+               name=conv_name_base + '2b', use_bias=False)(x)
+    x = BatchNormalization(epsilon=eps, axis=bn_axis,
+                           name=bn_name_base + '2b')(x)
+    x = Scale(axis=bn_axis, name=scale_name_base + '2b')(x)
+    x = Activation('relu', name=conv_name_base + '2b_relu')(x)
+
+    x = Conv2D(nb_filter3, (1, 1), name=conv_name_base +
+               '2c', use_bias=False)(x)
+    x = BatchNormalization(epsilon=eps, axis=bn_axis,
+                           name=bn_name_base + '2c')(x)
+    x = Scale(axis=bn_axis, name=scale_name_base + '2c')(x)
+
+    shortcut = Conv2D(nb_filter3, (1, 1), strides=strides,
+                      name=conv_name_base + '1', use_bias=False)(input_tensor)
+    shortcut = BatchNormalization(
+        epsilon=eps, axis=bn_axis, name=bn_name_base + '1')(shortcut)
+    shortcut = Scale(axis=bn_axis, name=scale_name_base + '1')(shortcut)
+
+    x = add([x, shortcut], name='res' + str(stage) + block)
+    x = Activation('relu', name='res' + str(stage) + block + '_relu')(x)
+    return x
+
+
+def resnet152_model(img_rows, img_cols, color_type=1, num_classes=None):
+    """
+    Resnet 152 Model
+
+    Keras implementation follows original Caffe implementation structure:
+    https://github.com/KaimingHe/deep-residual-networks
+
+    ImageNet Pretrained Weights:
+    https://drive.google.com/file/d/0Byy2AcGyEVxfeXExMzNNOHpEODg/view?usp=sharing
+
+    Args:
+      img_rows (int): row dimension of image inputs
+      img_cols (int): column dimension of image inputs
+      color_type (int): number of color channels (1 for greyscale, 3 for RGB)
+      num_classes (int): default 196, number of class labels for the classification task;
+      Stanford Cars Dataset is comprised of 196 car make-model-year labels. 
+      eps (float): default 1.0e-5, epsilon value to use in BatchNormalization layers
+
+    Returns:
+      compiled ResNet152 model
+    """
+    eps = 1.1e-5
+
+    global bn_axis
+    bn_axis = 3
+    img_input = Input(shape=(img_rows, img_cols, color_type), name='data')
+
+    x = ZeroPadding2D((3, 3), name='conv1_zeropadding')(img_input)
+    x = Conv2D(64, (7, 7), strides=(2, 2), name='conv1', use_bias=False)(x)
+    x = BatchNormalization(epsilon=eps, axis=bn_axis, name='bn_conv1')(x)
+    x = Scale(axis=bn_axis, name='scale_conv1')(x)
+    x = Activation('relu', name='conv1_relu')(x)
+    x = MaxPooling2D((3, 3), strides=(2, 2), name='pool1')(x)
+
+    x = conv_block(x, 3, [64, 64, 256], stage=2, block='a', strides=(1, 1))
+    x = identity_block(x, 3, [64, 64, 256], stage=2, block='b')
+    x = identity_block(x, 3, [64, 64, 256], stage=2, block='c')
+
+    x = conv_block(x, 3, [128, 128, 512], stage=3, block='a')
+    for i in range(1, 8):
+        x = identity_block(x, 3, [128, 128, 512], stage=3, block='b'+str(i))
+
+    x = conv_block(x, 3, [256, 256, 1024], stage=4, block='a')
+    for i in range(1, 36):
+        x = identity_block(x, 3, [256, 256, 1024], stage=4, block='b'+str(i))
+
+    x = conv_block(x, 3, [512, 512, 2048], stage=5, block='a')
+    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='b')
+    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='c')
+
+    x_fc = AveragePooling2D((7, 7), name='avg_pool')(x)
+    x_fc = Flatten()(x_fc)
+    x_fc = Dense(1000, activation='softmax', name='fc1000')(x_fc)
+
+    model = Model(img_input, x_fc)
+
+    weights_path = 'models/resnet152_weights_tf.h5'
+
+    model.load_weights(weights_path, by_name=True)
+
+    # Truncate and replace softmax layer for transfer learning
+    # Cannot use model.layers.pop() since model is not of Sequential() type
+    # The method below works since pre-trained weights are stored in layers but not in the model
+    x_newfc = AveragePooling2D((7, 7), name='avg_pool')(x)
+    x_newfc = Flatten()(x_newfc)
+    x_newfc = Dense(num_classes, activation='softmax', name='fc8')(x_newfc)
+
+    model = Model(img_input, x_newfc)
+
+    # Learning rate is changed to 0.001
+    sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(optimizer=sgd, loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    return model
+
+
+if __name__ == '__main__':
+    from keras.preprocessing.image import ImageDataGenerator
+    from keras.callbacks import CSVLogger, ModelCheckpoint, EarlyStopping
+    from keras.callbacks import ReduceLROnPlateau
+
+    img_width, img_height = 224, 224
+    num_channels = 3
+    train_data = '/Users/katerina/Workspace/visual_census/image_classifier/cars_train'
+    valid_data = '/Users/katerina/Workspace/visual_census/image_classifier/cars_test'
+    num_classes = 196
+    num_train_samples = 6549
+    num_valid_samples = 1595
+    verbose = 1
+    batch_size = 16
+    num_epochs = 10
+    patience = 50
+
+    # build a classifier model
+    model = resnet152_model(img_height, img_width, num_channels, num_classes)
+
+    # prepare data augmentation configuration
+    train_data_gen = ImageDataGenerator(rotation_range=20.,
+                                        width_shift_range=0.1,
+                                        height_shift_range=0.1,
+                                        zoom_range=0.2,
+                                        horizontal_flip=True)
+    valid_data_gen = ImageDataGenerator()
+
+    # callbacks
+    tensor_board = keras.callbacks.TensorBoard(
+        log_dir='./logs', histogram_freq=0, write_graph=True, write_images=True)
+    log_file_path = 'logs/training.log'
+    csv_logger = CSVLogger(log_file_path, append=False)
+    early_stop = EarlyStopping(monitor='val_accuracy', patience=patience)
+    reduce_lr = ReduceLROnPlateau(
+        monitor='val_accuracy', factor=0.1, patience=int(patience / 4), verbose=1)
+    trained_models_path = 'models/model'
+    model_names = trained_models_path + \
+        '.{epoch:02d}-{val_accuracy:.2f}.hdf5'
+    model_checkpoint = ModelCheckpoint(
+        model_names, monitor='val_accuracy', verbose=1, save_best_only=True)
+    callbacks = [tensor_board, model_checkpoint,
+                 csv_logger, early_stop, reduce_lr]
+
+    # generators
+    train_generator = train_data_gen.flow_from_directory(train_data, (img_width, img_height), batch_size=batch_size,
+                                                         class_mode='categorical')
+    valid_generator = valid_data_gen.flow_from_directory(valid_data, (img_width, img_height), batch_size=batch_size,
+                                                         class_mode='categorical')
+
+    # fine tune the model
+    model.fit_generator(
+        train_generator,
+        steps_per_epoch=num_train_samples / batch_size,
+        validation_data=valid_generator,
+        validation_steps=num_valid_samples / batch_size,
+        epochs=num_epochs,
+        callbacks=callbacks,
+        verbose=verbose)
---
+++ image_classifier/test_streetview.py
@@ -0,0 +1,82 @@
+# For loading the images and annotations
+import json
+import os
+import pickle
+
+# For preprocessing each image
+import cv2 as cv
+import keras.backend as K
+import numpy as np
+import scipy.io
+
+# For running our classifier
+from utils import load_model
+
+if __name__ == '__main__':
+    img_width, img_height = 224, 224
+    model = load_model()
+    model.load_weights('models/model.96-0.89.hdf5')
+
+    cars_meta = scipy.io.loadmat('devkit/cars_meta')
+    class_names = cars_meta['class_names']  # shape=(1, 196)
+    class_names = np.transpose(class_names)
+
+    test_path = '/Users/katerina/Workspace/visual_census/ucf_data/' #'/Users/katerina/Workspace/visual_census/ucf_data/'
+
+    # use either resnetv2 or mobilenet; resnet pickle should contain better results
+    with open('/Users/katerina/Workspace/visual_census/object_detector/ucf_objects_detected_inception_resnet_v2.pickle', 'rb') as f: #/Users/katerina/Workspace/visual_census/
+        ucf_objects_resnet = pickle.load(f)
+
+    # get the count of object instances in each scene (used to test heatmap)
+    object_counts = {}
+    # create empty list to write our results to
+    results = []
+
+    for key, value in ucf_objects_resnet.items():
+        # read in whole street view images
+        object_counts[key] = len(value)
+
+        # we can ignore scenes with no other car-type object instances
+        if len(value) > 0:
+            src_path = os.path.join(test_path, key)
+            print('Start processing image: {}'.format(key))
+            src_image = cv.imread(src_path)
+            #image_pil = Image.fromarray(np.uint8(src_image)).convert("RGB")
+            height, width = src_image.shape[:2]
+
+            # iterate over object instances in scene
+            for instance in range(len(value)):
+                # crop object instance with bbox
+                bbox = ucf_objects_resnet[key][instance]['bbox']
+                ymin, xmin, ymax, xmax = tuple(bbox)
+                ymax = height*(ymax - ymin)
+                xmax = width*(xmax - xmin)
+                ymin = ymin*height
+                xmin = xmin*width
+
+                margin = 16
+                x1 = int(max(0, xmin - margin))
+                y1 = int(max(0, ymin - margin))
+                x2 = int(min(xmax + margin, width) + x1)
+                y2 = int(min(ymax + margin, height) + y1)
+                crop_image = src_image[y1:y2, x1:x2]
+
+                # preprocess cropped image for classifier
+                dst_img = cv.resize(src=crop_image, dsize=(img_width, img_height), interpolation=cv.INTER_CUBIC)
+                rgb_img = cv.cvtColor(dst_img, cv.COLOR_BGR2RGB)
+                rgb_img = np.expand_dims(rgb_img, 0)
+
+                # make predictions
+                preds = model.predict(rgb_img)
+                prob = np.max(preds)
+                class_id = np.argmax(preds)
+                text = ('Predict: {}, prob: {}'.format(class_names[class_id][0][0], prob))
+                print(text)
+                results.append({'scene': key, 'label': class_names[class_id][0][0], 'prob': '{:.4}'.format(prob)})
+
+    # write results to json file
+    with open('results_resnet_v2.json', 'w') as file:
+        json.dump(results, file, indent=4)
+
+    # clear session to avoid clutter from old models / layers
+    K.clear_session()
